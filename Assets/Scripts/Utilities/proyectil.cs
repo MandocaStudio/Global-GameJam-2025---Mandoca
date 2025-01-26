308784bc0fae207323e0f99a207b631bd0107832
Christian Martinez <70175081+Chrisalex02@users.noreply.github.com>
2025-01-25 23:05:42 -0400

    main - christian - efectos proyectil

--- Assets/Scripts/Utilities/proyectil.cs
+++ Assets/Scripts/Utilities/proyectil.cs
@@ -3,13 +3,33 @@ using UnityEngine;
 
 public class proyectil : MonoBehaviour
 {
-    public float bulletDamage;
+    public float bulletEffect;
 
+    public bool disparo = false;
     private void Update()
     {
-
+        if (disparo.GetKeyDown("Fire2")){
+            //enemigo =  GameObject.FindGameObjectsWithTag("Enemy");
+            //disparo = true;
+            private void OnCollisionEnter(Collision other){
+                if (other.collider.CompareTag("Enemy")){
+                    enemigo.transform.position = Vector3 + 1;
+                }
+            }
+            
+        }
+         if (disparo.GetKeyDown("Fire3")){
+            //enemigo =  GameObject.FindGameObjectsWithTag("Enemy");
+            //disparo = true;
+            private void OnCollisionEnter(Collision other){
+                if (other.collider.CompareTag("Enemy")){
+                    enemigo.transform.position = Vector3 - 1;
+                }
+            }
+        }
     }
-
+    
+     
     // private void OnCollisionEnter(Collision other)
     // {
     //     if (other.collider.CompareTag("Enemy"))
@@ -30,9 +50,9 @@ public class proyectil : MonoBehaviour
     //     {
     //         Destroy(gameObject);
     //     }
-
+    // }
 
 }
 
 
-// }
+
