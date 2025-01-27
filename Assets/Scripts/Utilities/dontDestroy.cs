00cf3a02678eaf11b76fe6844dc3c049ee9061cf
JhonKen099 <125842622+JhonKen099@users.noreply.github.com>
2025-01-27 19:20:17 -0400

    main - jhonmar - cosas de musica

--- Assets/Scripts/Utilities/dontDestroy.cs
+++ Assets/Scripts/Utilities/dontDestroy.cs
@@ -4,10 +4,12 @@ using UnityEngine.SceneManagement;
 public class PersistentAudio : MonoBehaviour
 {
     private static PersistentAudio instance;
-    private int previousSceneIndex = -1; // Variable para almacenar el índice de la escena anterior
 
     private void Awake()
     {
+        // Verificar si la escena actual es la de índice 0
+
+
         // Si ya existe una instancia, destruye este objeto
         if (instance != null && instance != this)
         {
@@ -18,34 +20,14 @@ public class PersistentAudio : MonoBehaviour
         // Marca esta instancia como única y no destruir al cargar nueva escena
         instance = this;
         DontDestroyOnLoad(gameObject);
-
-        // Suscribirse al evento de carga de escenas
-        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    private void Update()
     {
-        // Obtén el índice de la escena cargada
-        int sceneIndex = scene.buildIndex;
-
-        // Puedes registrar o usar el índice como necesites
-        Debug.Log("La escena cargada tiene el índice: " + sceneIndex);
-
-        // Verifica si pasamos de la escena 3 a la escena 4
-        if (previousSceneIndex == 3 && sceneIndex == 4)
+        if (SceneManager.GetActiveScene().buildIndex == 0 || SceneManager.GetActiveScene().buildIndex == 1 || SceneManager.GetActiveScene().buildIndex == 4 || SceneManager.GetActiveScene().buildIndex == 6)
         {
-            Debug.Log("Se ha pasado de la escena 3 a la 4, destruyendo el objeto.");
             Destroy(gameObject);
+            return;
         }
-
-        // Guarda el índice de la escena actual como la escena anterior
-        previousSceneIndex = sceneIndex;
-    }
-
-    private void OnDestroy()
-    {
-        // Asegúrate de desuscribirte cuando el objeto sea destruido
-        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
-
